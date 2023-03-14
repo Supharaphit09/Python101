669ee496126d8d5ba95f2eb93be2bfb0703effd7
Supharaphit09 <114590778+Supharaphit09@users.noreply.github.com>
2023-03-14 20:57:16 +0700

    Add files via upload

--- GUI-Knowledge.py
+++ GUI-Knowledge.py
@@ -2,12 +2,29 @@ from tkinter import*
 from tkinter import ttk #นำเข้าเพื่อทำให้ปุ่มไม่นู่น
 from tkinter import messagebox #ตกแต่งหน้าจอแสดงผล
 
-GUI = Tk()
-GUI.title('บันทึกสิ่งที่ต้องทำ')
-GUI.geometry('500x500') 
+####################CSV####################
+import csv
 
-L1 = Label(GUI,text='TO DO LIST',font=('Arial Black',30),fg='White',bg='#FFCFCF')
-L1.place(x=120,y=50) 
+def writecsv(datalist):
+    with open('data.csv','a',encoding='utf-8',newline='') as file:
+        fw = csv.writer(file)
+        fw.writerow(datalist)
+        
+def readcsv():
+    with open('data.csv',encoding='utf-8',newline='') as file:
+        fr = csv.reader(file)
+        data = list(fr)
+    return data
+###########################################
+
+GUI = Tk()
+GUI.title('NOTE BY ME')
+GUI.geometry('500x1100') 
+
+L1 = Label(GUI,text='TO DO LIST',font=('Arial Black',30),fg='Black',bg='#F7F6CF',width=10,height=1)
+L1.place(x=120,y=80)
+
+GUI.configure(bg='#4F80C0')
 
 ##########
 def button2():
@@ -15,7 +32,7 @@ def button2():
         messagebox.showinfo('To Do',text)
 
 FB1=Frame(GUI)
-FB1=LabelFrame(GUI,text='อย่าขี้เกียจ!')
+FB1=LabelFrame(GUI,text='Fighting!',font=('Arial Black',10),fg='White',bg='#FBCCD0')
 FB1.place(x=200,y=200)
 B2 = ttk.Button(FB1,text='To Do List',command=button2)
 B2.pack(ipadx=5,ipady=5,padx=10,pady=10)
@@ -26,8 +43,8 @@ def button3():
         messagebox.showinfo('You can do it',text)
 
 FB2=Frame(GUI)
-FB2=LabelFrame(GUI,text='พยายามเข้า!')
-FB2.place(x=200,y=400)
+FB2=LabelFrame(GUI,text='Dont give up!',font=('Arial Black',10),fg='White',bg='#FBCCD0')
+FB2.place(x=200,y=350)
 B3 = ttk.Button(FB2,text='Finish',command=button3)
 B3.pack(ipadx=5,ipady=5,padx=10,pady=10)
 
@@ -37,8 +54,8 @@ def button4():
         messagebox.showinfo('Good job',text)
 
 FB3=Frame(GUI)
-FB3=LabelFrame(GUI,text='สิ่งที่ได้')
-FB3.place(x=200,y=600)
+FB3=LabelFrame(GUI,text='Good job!',font=('Arial Black',10),fg='White',bg='#FBCCD0')
+FB3.place(x=200,y=500)
 B4 = ttk.Button(FB3,text='Feedback',command=button4)
 B4.pack(ipadx=5,ipady=5,padx=10,pady=10)
 
@@ -48,9 +65,29 @@ def button5():
         messagebox.showinfo('Keep Fighting',text)
 
 FB4=Frame(GUI)
-FB4=LabelFrame(GUI,text='จดบันทึก')
-FB4.place(x=200,y=800)
+FB4=LabelFrame(GUI,text='Relax!',font=('Arial Black',10),fg='White',bg='#FBCCD0')
+FB4.place(x=200,y=650)
 B5 = ttk.Button(FB4,text='Note',command=button5)
 B5.pack(ipadx=5,ipady=5,padx=10,pady=10)
+
+###########
+LF1 = ttk.LabelFrame(GUI,text='Everyting that she like')
+LF1.place(x=155,y=800)
+
+v_data = StringVar()
+E1 = ttk.Entry(LF1,textvariable=v_data,font=('Arial Black',10))
+E1.pack(pady=5,padx=5)
+
+from datetime import datetime
+
+def SaveData():
+        t = datetime.now().strftime('%Y%m%d %H%M%S')
+        data = v_data.get()
+        text = [t,data]
+        writecsv(text)
+        v_data.set('')
+        
+B4 = ttk.Button(LF1,text='Save',command=SaveData)
+B4.pack(ipadx=10,ipady=10)
 
 GUI.mainloop()
